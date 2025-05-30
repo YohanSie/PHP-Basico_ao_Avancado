893b1e39c1d24eceeb0b4d7b612d17fcb2231ce1
Yohan Sie. <137341837+YohanSie@users.noreply.github.com>
2025-05-30 11:18:46 -0300

    30/05/25
    Coleta de dados de formulário - INSERT do MySQL com PDO

---
+++ PHPandMySQL/FormToDB/tratar.php
@@ -0,0 +1,33 @@
+<?php
+
+use sys4soft\Database;
+
+// importar a class Database e configurações
+require_once 'Database.php';
+
+// instanciação de objeto Database
+$config = [
+    'host' => 'localhost',
+    'database' => 'os_meus_clientes',
+    'username' => 'user_meus_clientes',
+    'password' => '45tA873IwA8a8aYIg53ud6lA6aQiTO',
+    'port' => '3308'
+];
+
+$database = new Database($config);
+
+// parametros
+$params = [
+    ':nome' => $_POST['text_nome'],
+    ':email' => $_POST['text_email']
+]; 
+
+// inserir os dados do cliente
+$result = $database->execute_non_query("INSERT INTO clientes(nome, email, created_at) VALUES (:nome, :email, NOW())", $params);
+
+echo "<pre>";
+print_r($result);
+echo "</pre>";
+
+// voltar ao formulário automaticamente
+header("Location: formulario.php");
