359248c3ead39c48832304d8d9259168657e91d1
Yohan Sie. <137341837+YohanSie@users.noreply.github.com>
2025-07-21 20:31:37 -0300

    db config

--- project_BNG/app/config.php
+++ project_BNG/app/config.php
@@ -2,4 +2,10 @@
 
 define('APP_NAME', 'Basic Name Gathering');
 
+// Database
+define('MYSQL_HOST', 'localhost');
+define('MYSQL_DATABASE', 'db_bng');
+define('MYSQL_USERNAME', 'user_db_bng');
+define('MYSQL_PASSWORD', '3iohgrt23o527oigi');
+
 ?>
