032e7c81a6af12f87855f01877a2fc693bd0d584
Yohan Sie. <137341837+YohanSie@users.noreply.github.com>
2025-07-23 19:06:53 -0300

    criação pasta BNG

---
+++ BNG/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'sys4soft/bng',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '791045297e5f22b97658941791586951643ea873',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'sys4soft/bng' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '791045297e5f22b97658941791586951643ea873',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
